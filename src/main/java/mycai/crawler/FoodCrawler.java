d776f1bd5a96f18dec9dbf7ac8ef6ab74e2b9903
tangld <tangld@ctrip.com>
2015-05-26 17:54:06 +0800

    start to write food crawler

--- src/main/java/mycai/crawler/FoodCrawler.java
+++ src/main/java/mycai/crawler/FoodCrawler.java
@@ -2,6 +2,7 @@ package mycai.crawler;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
@@ -11,14 +12,36 @@ import java.io.IOException;
  */
 public class FoodCrawler {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        String host = "http://www.21food.cn/quote/";
         try {
-            Document doc = Jsoup.connect("http://www.21food.cn/quote/").get();
-            System.out.println(doc.title());
-            Elements eles = doc.select("div.artHead");
-            System.out.println(eles.first().select("h3[class=artTitle]"));
+            Document doc = Jsoup.connect(host).get();
+            Elements eles = doc.select("div.price-bg").select("ul.pfl-ul>li>a");
+            for (Element element : eles) {
+                extractFoodInfo(element.text(), host + element.attr("href").toString());
+                break;
+            }
+//            System.out.println(eles);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void extractFoodInfo(String text, String href) {
+        try {
+            System.out.println(text + " - " + href);
+            Document doc = Jsoup.connect(href).get();
+            Elements elements = doc.select(".city-integrate table tr");
+            for (int i = 1; i < elements.size(); i++) {
+                Elements eles = elements.get(i).select("td");
+                for (Element e : eles) {
+                    System.out.println(e);
+
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
---
+++ src/main/java/mycai/crawler/FoodItem.java
@@ -0,0 +1,12 @@
+package mycai.crawler;
+
+/**
+ * Created by tangld on 2015/5/26.
+ */
+public class FoodItem {
+    String name;
+    String brand;
+    String price;
+    String companyName;
+    String date;
+}
