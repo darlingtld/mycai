c66ede05df776e1522fd807cf9ceffc492b053bd
darlingtld <darlingtld@qq.com>
2015-05-30 16:31:18 +0800

    saturday.  i just wrote a crawler to fetch vegetable information

--- src/main/java/mycai/pojo/Category.java
+++ src/main/java/mycai/pojo/Category.java
@@ -4,6 +4,7 @@ package mycai.pojo;
  * Created by darlingtld on 2015/5/16.
  */
 public enum Category {
+//  ----SHUCAISHUIGUO("蔬菜水果"),
     YECAILEI("叶菜类"),
     GENJINGLEI("根茎类"),
     QIEGUOLEI("茄果类"),
@@ -12,6 +13,7 @@ public enum Category {
     JUNLEI("菌类"),
     TECAI("特菜"),
 
+//    QINROUDANLEI("禽肉蛋类"),
     YIJIBAITIAO("一级白条"),
     ERJIBAITIAO("二级白条"),
     DONGZHUROU("冻猪肉"),
@@ -24,9 +26,11 @@ public enum Category {
     QINDANLEI("禽蛋类"),
     SHUSHIJIAGONG("熟食加工"),
 
+//    SHUICHANDONGHUO("水产冻货"),
     HAIXIANSHUICHAN("海鲜水产"),
     YUWANHUOGUO("鱼丸火锅"),
 
+//    MIMIANLIANGYOU("米面粮油"),
     DAMI("大米"),
     MIANFEIMIANTIAO("面粉面条"),
     SHIYONGYOU("食用油"),
@@ -34,6 +38,7 @@ public enum Category {
     MIANDIAN("面点"),
     HONGBEIZUOLIAO("烘焙佐料"),
 
+//    TIAOLIAOQITA("调料其他"),
     TIAOWEIPIN("调味品"),
     FENSIFENTIAO("粉丝粉条"),
     DOUZHIPIN("豆制品"),
@@ -42,9 +47,11 @@ public enum Category {
     JIANGYOUCU("酱油醋"),
     YANCAIGUANTOU("腌菜罐头"),
 
+//    CANCHUYONGPIN("餐厨用品"),
     ZHIPINSHIJIN("纸品湿巾"),
     CANYINYONGJU("餐饮用具"),
 
+//    JIUSHUIYINLIAO("酒水饮料");
     YINLIAO("饮料"),
     YINYONGSHUI("饮用水");
 
