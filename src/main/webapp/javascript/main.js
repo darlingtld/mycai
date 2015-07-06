4ee0ac9661cc36c02d88ed6c9432c6e896eb5ae2
darlingtld <darlingtld@qq.com>
2015-07-06 10:07:17 +0800

    first push

--- src/main/webapp/javascript/main.js
+++ src/main/webapp/javascript/main.js
@@ -178,71 +178,19 @@ var pageStatus = {
 var curStatus = pageStatus.first_open;
 
 mycaiModule.config(function(){
-
+// Check browser support
+    if (typeof(Storage) != "undefined") {
+        // Store
+        localStorage.setItem("lastname", "Smith");
+        console.log(localStorage.getItem("lastname"));
+    } else {
+        console.log("local storage is not supported!")
+    }
 });
 
 
 mycaiModule.controller('mainController', function () {
-    //curStatus = pageStatus.first_open;
     getUserInfo();
-    //var startX, curX, startY, curY; // Variables
-    //var newXScroll, newYScroll, genXScroll; // More Variables!
-    //
-    //// Change the height of the sidebar, as well as a few things to do with the main content area, so the user
-    //// can actually scroll in the content area.
-    //function sideBarHeight() {
-    //
-    //    var docHeight = $(document).height();
-    //    var winHeight = $(window).height();
-    //
-    //    $('.slide-in').height(winHeight);
-    //    $('#main-container').height(winHeight);
-    //    $('#sub-container').height($('#sub-container').height());
-    //}
-    //
-    //sideBarHeight();
-    //
-    //var outIn = 'in';
-    //
-    //Hammer(document.getElementById('main-container')).on('swiperight', function (e) {
-    //    $('.slide-in').toggleClass('on');
-    //    $('#main-container').toggleClass('on');
-    //    outIn = 'out';
-    //
-    //});
-    //
-    //Hammer(document.getElementById('main-container')).on('swipeleft', function (e) {
-    //    $('.slide-in').toggleClass('on');
-    //    $('#main-container').toggleClass('on');
-    //    outIn = 'in';
-    //});
-    //
-    //
-    //function runAnimation() {
-    //
-    //    if (outIn == 'out') {
-    //
-    //        $('.slide-in').toggleClass('on');
-    //        $('#main-container').toggleClass('on');
-    //        outIn = 'in';
-    //
-    //    } else if (outIn == 'in') {
-    //
-    //        $('.slide-in').toggleClass('on');
-    //        $('#main-container').toggleClass('on');
-    //        outIn = 'out';
-    //
-    //    }
-    //}
-    //
-    //$('.menu-icon')[0].addEventListener('touchend', function (e) {
-    //    $('.slide-in').toggleClass('on');
-    //    $('#main-container').toggleClass('on');
-    //});
-    //$('.menu-icon').click(function () {
-    //    $('.slide-in').toggleClass('on');
-    //    $('#main-container').toggleClass('on');
-    //});
 });
 
 function getUserInfo() {
@@ -405,6 +353,7 @@ mycaiModule.directive('spinnerInstance', function () {
             }
             element.css('width', '20%');
             element.css('text-align', 'center');
+            element.css('color','green');
             element.css('background-color', 'white');
             element.spinner({
                 value: value,
